40cbe2b10285d1ed97c1718b02a1c2ea1d9429b7
deepaksharmadk <dks847203@gmail.com>
2024-05-13 00:00:16 +0530

    bug fix

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,23 +1,33 @@
-// Append the menu for mobile
+// // Append the menu for mobile
 let menu = document.getElementById("menu");
-// console.log(menu);
+
 let contact = document.getElementById("contact");
 let mobMenu = document.querySelector("#mobMenus .mobMenu");
 let newMenu = document.createElement("div");
 let newContact = document.createElement("div");
 newMenu.innerHTML = menu.innerHTML;
-newContact.innerHTML = contact.innerHTML;
-mobMenu.appendChild(newMenu.firstElementChild);
-// mobMenu.appendChild(newContact.firstElementChild);
+let left = newContact.innerHTML;
+// console.log(left);
+
+// let right = contact.innerHTML;
+// console.log(right);
+// let g = (newContact.innerHTML = contact.innerHTML);
+// left = right;
+// // console.log("hello", g);
+let g = newMenu.firstElementChild;
+mobMenu.appendChild(g);
+
+mobMenu.append(newContact.firstElementChild);
 
 // Menu Toggle Open and Close
 let menuOpen = document.getElementById("menuOpen");
-console.log(menuOpen);
+
 let menuClose = document.getElementById("menuClose");
+
 let menuOverlay = document.getElementById("menuOverlay");
 menuOpen.addEventListener("click", () => {
   mobMenu.classList.toggle("active");
-  console.log("hello");
+
   menuOverlay.classList.toggle("active");
 });
 menuClose.addEventListener("click", () => {
@@ -28,24 +38,3 @@ menuOverlay.addEventListener("click", () => {
   mobMenu.classList.toggle("active");
   menuOverlay.classList.toggle("active");
 });
-
-// Text Write Animation
-
-// function iterator(element) {
-//   nextIndex = 0;
-//   return {
-//     next: function () {
-//       if (nextIndex < element.length) {
-//         return {
-//           value: element[nextIndex++],
-//           done: false,
-//         };
-//       } else {
-//         nextIndex = 0;
-//         return {
-//           done: true,
-//         };
-//       }
-//     },
-//   };
-// }
